0ee02d98e1515d69bae340ae8cbab1011c8d0797
adrian gutkowski <adrianvxc@gmail.com>
2019-03-28 19:02:28 +0100

    list files from bucket

--- cli.js
+++ cli.js
@@ -1,3 +1,8 @@
 import AWS from 'aws-sdk';
-import uuid from 'node-uuid';
+
+let client = new AWS.S3();
+client.listObjects({}, (err, data) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+});
 
