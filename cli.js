176dd30a6e0012fa784fc3bcbe988f405b7c65e5
adrian gutkowski <adrianvxc@gmail.com>
2019-03-28 19:10:19 +0100

    update list of all file

--- cli.js
+++ cli.js
@@ -2,7 +2,11 @@ import AWS from 'aws-sdk';
 
 let client = new AWS.S3();
 client.listObjects({Bucket: 'lcloud-427-ag'}, (err, data) => {
-    if (err) console.log(err, err.stack);
-    else console.log(data);
+    if (err) {
+        console.log(err, err.stack);
+    } else {
+        console.log('all files for bucket lcloud-427-ag');
+        console.log(data);
+    }
 });
 
